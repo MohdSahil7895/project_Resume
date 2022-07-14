c9ffd8c46e1eed83b65f112682d2e50e27992b3e
MohdSahil7895 <>
2022-07-14 17:08:44 +0530

    Added Code 1

--- src/My Components/dasfs.js
+++ src/My Components/dasfs.js
@@ -1,1 +1,12 @@
-njhjhghgjgjyg
+let loginstyle={
+    padding:"50px",
+    margin: "auto",
+    alignItems: 'center',
+    justifyContent:'center',
+    height:"50%",
+    width:"50%",
+    border:"1px solid black",
+    paddingTop: '50%',
+    }
+
+    // className="container p-3 my-3  center" style={loginstyle}
